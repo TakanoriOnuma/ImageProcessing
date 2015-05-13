387d39b396f2d12a9855ddddb57864f7950c7c1c
Takanori Onuma <s1000607@sendai-nct.jp>
2015-05-13 12:13:02 +0900

    矩形波形でfilter関数を通した
    矩形波形を定義して、filter関数に通した

--- imgProcessing.cpp
+++ imgProcessing.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <stdio.h>
 #include <typeinfo>
+#include <math.h>
 
 #include "ToneCurve.h"
 #include "Image.h"
 
 using namespace std;
 
+double square(int x, int f, int T)
+{
+    int sign = 1;
+    double interval = (double)T / (2 * f);
+    while(x > interval) {
+        x -= interval;
+        sign *= -1;
+    }
+    return sign;
+}
+
 double filter(int i, int j, int height, int width)
 {
-    return (double)255 * j / width;
+    return 30 * square(i, 8, height) +
+        30 * square(j, 8, width);
 }
 
 int main()
 {
-    Image* img = new Image(256, 256, 0);
+    Image* img = new Image(256, 256, 100);
     img->filter(filter);
-    img->save("left_lenear.pgm");
+    img->save("squarewave.pgm");
 
     delete img;
     return 0;
