34f8425aae522b73c2c6fcb5b4207e3208fde08b
Takanori Onuma <s1000607@sendai-nct.jp>
2015-06-23 11:14:34 +0900

    ビットリバースの実現
    ビットリバースを実現し、
    FFTを実装した。

--- fft.cpp
+++ fft.cpp
@@ -3,6 +3,26 @@
 
 using namespace std;
 
+int reverse(unsigned int x, int bitNum)
+{
+    unsigned int rev = 0;
+    for(int i = 0; i < bitNum; i++) {
+        rev = (rev << 1) | (x & 1);
+        x = x >> 1;
+    }
+    return rev;
+}
+
+int calcPower(int x, int base)
+{
+    int index = 0;
+    while(x != 1) {
+        x /= base;
+        ++index;
+    }
+    return index;
+}
+
 vector< complex<double> > fft(const vector< complex<double> >& vec)
 {
     vector< complex<double> > w(vec.size());
@@ -19,7 +39,6 @@ vector< complex<double> > fft(const vector< complex<double> >& vec)
     for(int step = vec.size() / 2; step >= 1; step /= 2, b++) {
         int idx = 0;
         while(idx < vec.size()) {
-            cout << "idx:" << idx << endl;
             for(int n = 0; n < step; n++, idx++) {
                 complex<double> buf = fftVec[idx] + fftVec[idx + step];
                 fftVec[idx + step] = w[b * n] * (fftVec[idx] - fftVec[idx + step]);
@@ -27,14 +46,16 @@ vector< complex<double> > fft(const vector< complex<double> >& vec)
             }
             idx += step;
         }
-
-        cout << "---------- step" <<  b << " ------------" << endl;
-        for(int i = 0; i < fftVec.size(); i++) {
-            cout << fftVec[i] << endl;
-        }
     }
 
     // ビットリバース
+    int bitNum = calcPower(fftVec.size(), 2);
+    for(int i = 0; i < fftVec.size(); i++) {
+        unsigned int rev = reverse((unsigned int)i, bitNum);
+        if(rev < fftVec.size() / 2) {
+            swap(fftVec[i], fftVec[rev]);
+        }
+    }
 
     return fftVec;
 }
